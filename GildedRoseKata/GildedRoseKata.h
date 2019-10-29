72dcb5c034f6ca9367f4d6055fb94039333db0b1
Patrick <patrickdiviacco@users.noreply.github.com>
2019-10-29 10:20:20 +0100

    Initial Commit

---
+++ GildedRoseKata/GildedRoseKata.h
@@ -0,0 +1,19 @@
+//
+//  GildedRoseKata.h
+//  GildedRoseKata
+//
+//  Created by Patrick Diviacco on 29/10/2019.
+//  Copyright © 2019 Patrick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GildedRoseKata.
+FOUNDATION_EXPORT double GildedRoseKataVersionNumber;
+
+//! Project version string for GildedRoseKata.
+FOUNDATION_EXPORT const unsigned char GildedRoseKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GildedRoseKata/PublicHeader.h>
+
+
