bf5872c1bf9daf98717fefddfd6c18857923e22d
Patrick <patrickdiviacco@users.noreply.github.com>
2019-10-30 11:43:59 +0100

    Fixed Quality Tests bug, and refactored Strategies to pass tests

--- GildedRoseKata/ItemStrategy.swift
+++ GildedRoseKata/ItemStrategy.swift
@@ -13,14 +13,27 @@ protocol ItemStrategy {
     func updateSellIn(item: Item)
 }
 
+extension ItemStrategy {
+    func adjustQualityLowerBound(item: Item) {
+        if item.quality < 0 {
+            item.quality = 0
+        }
+    }
+    
+    func adjustQualityUpperBound(item: Item) {
+        if item.quality > 50 {
+            item.quality = 50
+        }
+    }
+}
+
 class NormalItemStrategy : ItemStrategy {
     func updateQuality(item: Item) {
-        if item.quality > 0 {
-            switch item.sellIn {
-            case Int.min...0: item.quality -= 2
-            default:  item.quality -= 1
-            }
+        switch item.sellIn {
+        case Int.min...0: item.quality -= 2
+        default: item.quality -= 1
         }
+        adjustQualityLowerBound(item: item)
     }
     
     func updateSellIn(item: Item) {
@@ -30,9 +43,8 @@ class NormalItemStrategy : ItemStrategy {
 
 class BrieItemStrategy : ItemStrategy {
     func updateQuality(item: Item) {
-        if item.quality < 50 {
-             item.quality += 1
-        }
+        item.quality += 1
+        adjustQualityUpperBound(item: item)
     }
     
     func updateSellIn(item: Item) {
@@ -47,14 +59,14 @@ class SulfurasItemStrategy : ItemStrategy {
 
 class BackstageItemStrategy : ItemStrategy {
     func updateQuality(item: Item) {
-        if item.quality < 50 {
-            switch item.sellIn {
-            case Int.min...0: item.quality = 0
-            case 0..<6: item.quality += 3
-            case 6..<11: item.quality += 2
-            default: item.quality += 1
-            }
+        switch item.sellIn {
+        case Int.min...0: item.quality = 0
+        case 0..<6: item.quality += 3
+        case 6..<11: item.quality += 2
+        default: item.quality += 1
         }
+        adjustQualityLowerBound(item: item)
+        adjustQualityUpperBound(item: item)
     }
     
     func updateSellIn(item: Item) {
@@ -64,12 +76,11 @@ class BackstageItemStrategy : ItemStrategy {
 
 class ConjuredItemStrategy : ItemStrategy {
     func updateQuality(item: Item) {
-        if item.quality > 0 {
-            switch item.sellIn {
-            case Int.min...0: item.quality -= 4
-            default:  item.quality -= 2
-            }
+        switch item.sellIn {
+        case Int.min...0: item.quality -= 4
+        default: item.quality -= 2
         }
+        adjustQualityLowerBound(item: item)
     }
     
     func updateSellIn(item: Item) {
--- GildedRoseKataTests/AllItemsTests.swift
+++ GildedRoseKataTests/AllItemsTests.swift
@@ -20,24 +20,24 @@ class AllItemsTests : XCTestCase {
         XCTAssertEqual(sut.items.count, 0, "Items array should be empty")
     }
 
-    func testUpdateQualityMoreOrEqualTo0() throws {
+    func testUpdateQualityMoreOrEqualTo0() {
         let input = Seeds.allItems
         let sut = GildedRose(items: input)
         sut.updateQuality()
-        XCTAssertGreaterThanOrEqual(sut.items.first!.quality, 0, "Quality should be >= 0")
+        XCTAssertTrue(sut.items.allSatisfy { $0.quality >= 0 }, "Quality should be >= 0")
     }
         
     func testUpdateQualityNearMin() {
         let input = Seeds.itemsWithQualityEqual1
         let sut = GildedRose(items: input)
         sut.updateQuality()
-        XCTAssertEqual(sut.items.first!.quality, 0, "Quality should be 0")
+        XCTAssertTrue(sut.items.allSatisfy { $0.quality >= 0 }, "Quality should be >= 0")
     }
     
     func testUpdateQualityAtMin() {
         let input = Seeds.itemsWithQualityEqual0
         let sut = GildedRose(items: input)
         sut.updateQuality()
-        XCTAssertEqual(sut.items.first!.quality, 0, "Quality should be 0")
+        XCTAssertTrue(sut.items.allSatisfy { $0.quality >= 0 }, "Quality should be >= 0")
     }
 }
