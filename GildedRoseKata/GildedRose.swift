551c10dc595a2757817cb21ad18c90b4c6ca3f47
Patrick <patrickdiviacco@users.noreply.github.com>
2019-10-30 08:56:00 +0100

    Extract Brie item logic

--- GildedRoseKata/GildedRose.swift
+++ GildedRoseKata/GildedRose.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2019 Patrick. All rights reserved.
 //
 
+enum Names : String {
+    case brie = "Aged Brie"
+    case sulfuras = "Sulfuras, Hand of Ragnaros"
+    case backstage = "Backstage passes to a TAFKAL80ETC concert"
+    case conjured = "Conjured Mana Cake"
+}
+
 public class GildedRose {
     var items:[Item]
     
@@ -14,55 +21,66 @@ public class GildedRose {
     }
     
     public func updateQuality() {
-        
-        for i in 0..<items.count {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
+        for item in items {
+            if item.name == Names.brie.rawValue {
+                item.sellIn -= 1
+                if item.quality < 50 {
+                    item.quality += 1
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                    
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                        
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
+                updateQuality(item: item)
+            }
+        }
+     }
+        
+    private func updateQuality(item: Item) {
+        if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.quality > 0) {
+                if (item.name != "Sulfuras, Hand of Ragnaros") {
+                    item.quality = item.quality - 1
                 }
             }
-            
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-            
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+                
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
                     }
                 }
             }
         }
+        
+        if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.sellIn = item.sellIn - 1
+        }
+        
+        if (item.sellIn < 0) {
+            if (item.name != "Aged Brie") {
+                if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.quality > 0) {
+                        if (item.name != "Sulfuras, Hand of Ragnaros") {
+                            item.quality = item.quality - 1
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
+        
     }
 }
